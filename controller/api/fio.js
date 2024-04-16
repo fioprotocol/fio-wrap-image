f49e6f4e4da3b43fda7419862947e0bd416da5f0
Kyryl Petkov <kirill.petkov@ditto.trade>
2024-04-16 14:19:05 +0400

    DASH-994 add handlers support

--- controller/api/fio.js
+++ controller/api/fio.js
@@ -1,82 +1,34 @@
-const { readFileSync } = require('fs')
-let draw1 = '<rect id="Rectangle" fill="#FFFFFF" opacity="0.05" x="15" y="160" width="220" height="37" rx="7"></rect><text id=".d" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="ProximaNova-Medium, Proxima Nova" font-size="15" font-weight="400" line-spacing="19" fill="#FFFFFF"><tspan x="118" y="183"></tspan></text>';
-let draw2 = '<rect id="Rectangle" fill="#FFFFFF" opacity="0.05" x="15" y="160" width="220" height="54" rx="7"></rect><text id=".domainnamedomainnam"  font-family="ProximaNova-Medium, Proxima Nova" font-size="15" font-weight="400" line-spacing="19" fill="#FFFFFF"><tspan x="31" y="182"></tspan><tspan x="31" y="201"></tspan></text>';
-let draw3 = '<rect id="Rectangle" fill="#FFFFFF" opacity="0.05" x="15" y="160" width="220" height="75" rx="7"></rect><text id=".domainnamedomainnam" font-family="ProximaNova-Medium, Proxima Nova" font-size="15" font-weight="400" line-spacing="19" fill="#FFFFFF"><tspan x="32" y="183"></tspan><tspan x="32" y="202"></tspan><tspan x="32" y="221"></tspan></text>';
-class FIOCtrl {
-    constructor() {}
-    
-    async getImageUrlWithDomain(req,res) {
-      let domain = req.params.domain;
-      let realdomain = domain;
-      if (domain.indexOf(".svg") > 1) {
-        let index = domain.indexOf(".svg");
-        realdomain = domain.substring(0,  index);
-      }
-      try {
-          let tempRes = await this.generateImage(realdomain);
-          res.send(tempRes);
-        } catch (error) {
-          res.send({})
-        }
-    }
-    async returnSVG() {
-      const path = "fio.svg";
-      const data = readFileSync(path)
-      return data.toString();
-    }
-    async generateImage(domain) {
-      let realDomain = "@"+domain
-      let domainLength = realDomain.length;
-      let type = 0;
-      if (domainLength < 22) type = 1;
-      else if(domainLength >21 && domainLength<43) type = 2;
-      else if(domainLength>42&&domainLength<64) type =3;
-      let drawString = await this.generateRect(type, realDomain);
-      let string = await this.returnSVG();
-      let output = await this.drawDomain(type, string, drawString);
-      return output;
-    }
-    async drawDomain(type, fioString, drawString) {
-      if (type == 1 || type == 3) {
-        let str = '250"></polygon>';
-        let index = fioString.indexOf(str)+str.length;
-        var output = fioString.substring(0, index) + drawString + fioString.substring(index);
-        return output;
-      } else if (type == 2) {
-        let str = '250"></polygon>';
-        let index = fioString.indexOf(str)+str.length;
-        var output = fioString.substring(0, index) + drawString + fioString.substring(index);
-        return output;
-      } else {
-        return fioString;
-      }
-    }
-    async generateRect(type, realDomain) {
-      if (type == 1) {
-        const index = draw1.search("</tspan>");
-        let newDraw = draw1;
-        var output = newDraw.substring(0, index) + realDomain + newDraw.substring(index);
-        return output;
-      } else if (type == 2) {
-        let index = draw2.indexOf("</tspan>");
-        let newDraw = draw2;
-        var output = newDraw.substring(0, index) + realDomain.substring(0,21) + newDraw.substring(index);
-        index = output.indexOf("</tspan>", index+22);
-        output = output.substring(0,index)+ realDomain.substring(21) + output.substring(index);
-        return output;
-      } else if (type == 3) {
-        let index = draw3.indexOf("</tspan>");
-        let newDraw = draw3;
-        var output = newDraw.substring(0, index) + realDomain.substring(0,21) + newDraw.substring(index);
-        index = output.indexOf("</tspan>", index+22);
-        output = output.substring(0,index)+ realDomain.substring(21,42) + output.substring(index);
-        index = output.indexOf("</tspan>", index+22);
-        output = output.substring(0,index)+ realDomain.substring(42) + output.substring(index);
-        return output;
-      } else {
-        return "";
-      }
-    }
+const {readFileSync} = require('fs');
+
+export const handler = (req, res) => res.send(generateSvg(req.params.domain.replace('.svg', '')));
+
+const fioSvg = readFileSync("fio.svg").toString()
+
+const generateSvg = (name) => {
+    const isDomain = name.indexOf('@') === -1;
+    const realName = isDomain ? `@${name}` : name;
+    return fioSvg.replace('{content}', generateContent(realName));
 }
 
-export default new FIOCtrl();
+const generateContent = (realName) => {
+    const topOffset = 160;
+    const verticalPadding = 8;
+
+    const fontSize = 15;
+    const lineHeight = fontSize * 1.45;
+
+    const symbolsPerLine = 21;
+    const lines = Math.ceil(realName.length / symbolsPerLine);
+
+    const rectHeight = verticalPadding * 2 + lineHeight * lines;
+
+    let content = `<rect id="Rectangle" fill="#FFFFFF" opacity="0.05" x="15" y="${topOffset}" width="220" height="${rectHeight}" rx="7"/>`;
+
+    for (let i = 0; i < lines; i++) {
+        const textTopOffset = topOffset + verticalPadding + fontSize + i * lineHeight;
+        const namePart = realName.substring(i * symbolsPerLine, (i * symbolsPerLine) + symbolsPerLine);
+        content += `<text font-family="ProximaNova-Medium, Proxima Nova" font-size="${fontSize}" font-weight="400" fill="#FFFFFF" x="${lines === 1 ? "50%" : 30}" text-anchor="${lines === 1 ? "middle" : "left"}" y="${textTopOffset}">${namePart}</text>`;
+    }
+
+    return content;
+}
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express();
+const cors = require("cors");
 const bodyParser = require('body-parser')
-import mainCtrl from './controller/main';
-app.use(
-  express.urlencoded({
-    extended: true
-  })
-)
+const {handler} = require("./controller/api/fio");
 
-app.use(express.json())
-app.use(bodyParser.json())
-mainCtrl.start(app).catch(console.error);
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors({ origin: "*" }));
+
+app.get("/nftimage/:domain", handler);
+
 app.listen(3010, () => {
-  console.log(`server listening on *: 3010`)
+    console.log(`server listening on *: 3010`)
 });
